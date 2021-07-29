db2711e9e23f369223aeb3d45424544fec830855
Peter Heitkemper <71290233+pmh-3@users.noreply.github.com>
2021-07-29 09:32:17 -0700

    Add files via upload

---
+++ Actions.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace GreenList
+{
+
+    public class Actions {
+
+        
+        public bool createDB(SQLiteCommand cmd)
+        {
+
+
+            cmd.CommandText = @"CREATE TABLE Items(id INTEGER PRIMARY KEY,
+                name TEXT, quantity INT, meal TEXT, list TEXT, section TEXT, price REAL)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE Meals(id INTEGER PRIMARY KEY,
+                name TEXT, list TEXT, price REAL)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE Lists(id INTEGER PRIMARY KEY,
+                name TEXT, notes TEXT, date TEXT, price REAL)";
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Tables Created");
+
+            return true;
+        }
+
+        public bool addItems(SQLiteCommand cmd, string n, int q, string m, string l, string s, float p )
+        {
+            cmd.CommandText = "INSERT INTO Items(name, quantity, meal, list, section, price) VALUES(@name, @quantity, @meal, @list, @section, @price)";
+
+
+            cmd.Parameters.AddWithValue("@name", n);
+            cmd.Parameters.AddWithValue("@quantity", q);
+            cmd.Parameters.AddWithValue("@meal", m);
+            cmd.Parameters.AddWithValue("@list", l);
+            cmd.Parameters.AddWithValue("@section", s);
+            cmd.Parameters.AddWithValue("@price", p);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("item inserted");
+
+            return true;
+        }
+
+        public bool addMeals(SQLiteCommand cmd, string n, string l, float p)
+        {
+            cmd.CommandText = "INSERT INTO Meals(name, list, price) VALUES(@name, @list, @price)";
+
+            cmd.Parameters.AddWithValue("@name", n);
+
+            cmd.Parameters.AddWithValue("@list", l);
+            cmd.Parameters.AddWithValue("@price", p);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("meal inserted");
+
+            return true;
+        }
+
+        public bool addList(SQLiteCommand cmd, string n, string no, string d, float p)
+        {
+            cmd.CommandText = "INSERT INTO Lists(name, notes, date, price) VALUES(@name, @notes, @date, @price)";
+
+            cmd.Parameters.AddWithValue("@name", n);
+            cmd.Parameters.AddWithValue("@notes", no);
+            cmd.Parameters.AddWithValue("@date", d);
+            cmd.Parameters.AddWithValue("@price", p);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("List inserted");
+            return true;
+        }
+
+        public bool readItems(SQLiteCommand cmd)
+        {
+            cmd.CommandText = "SELECT * FROM Items LIMIT 5";
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            Console.WriteLine($"{rdr.GetName(0),-3} {rdr.GetName(1),-8} {rdr.GetName(2),8}");
+
+            while (rdr.Read())
+            {
+                Console.WriteLine($@"{rdr.GetInt32(0),-3} {rdr.GetString(1),-8} {rdr.GetInt32(2),8}");
+            }
+            return true;
+        }
+        public void readLists(SQLiteCommand cmd)
+        {
+            cmd.CommandText = "SELECT * FROM Lists";
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            Console.WriteLine($"{rdr.GetName(0),-3} {rdr.GetName(1),-8} {rdr.GetName(2),8} {rdr.GetName(3),12} {rdr.GetName(3),16}");
+
+            while (rdr.Read())
+            {
+                Console.WriteLine($@"{rdr.GetInt32(0),-3} {rdr.GetString(1),-8} {rdr.GetString(2),8} {rdr.GetString(3),12}{ rdr.GetFloat(4),16} ");
+            }
+
+        }
+
+
+        public void readListItems(SQLiteCommand cmd, string ListTitle)
+        {
+            cmd.CommandText = "SELECT * FROM Items WHERE list = 'mixers'";
+            //+ListTitle;
+            using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+            Console.WriteLine($"{rdr.GetName(0),-3} {rdr.GetName(1),-8} {rdr.GetName(2),8} {rdr.GetName(3),12} {rdr.GetName(4),16} {rdr.GetName(5),20} {rdr.GetName(6),24}");
+
+            while (rdr.Read())
+            {
+                Console.WriteLine($@"{rdr.GetInt32(0),-3} {rdr.GetString(1),-8} {rdr.GetInt32(2),8} {rdr.GetValue(3),12}{rdr.GetString(4),16} {rdr.GetString(5),20} {rdr.GetFloat(6),24}");
+            }
+
+        }
+
+    }
+
+}
+
+
---
+++ GreenList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using static GreenList.Actions;
+using GreenList;
+
+
+class Driver
+{
+    static int Main()
+    {
+        string[] sections = { "Produce", "Meat", "Dairy", "Bakery", "Frozen", "Other" };
+
+        string saveOption = "n";
+        string exitOption = "n";
+        bool isCreated = false;
+
+        //testing purposes
+        if (File.Exists(@"C:\Users\Peter\GreenList\Grocery.db"))
+        {
+            isCreated = true;
+        }
+
+        string cs = @"URI=file:C:\Users\Peter\GreenList\Grocery.db";
+        using var con = new SQLiteConnection(cs);
+        con.Open();
+        using var cmd = new SQLiteCommand(con);
+        Actions myActions = new Actions();
+
+        if (!isCreated)
+        {
+            myActions.createDB(cmd);
+        }
+
+        Console.WriteLine("Welcome to Green List!");
+
+        while (exitOption == "n")
+        {
+            Console.WriteLine("Would you like to create a NEW grocery list or OPEN an existing one?");
+
+            string listOption = Console.ReadLine();
+
+            if (listOption == "NEW")
+            {
+                Console.WriteLine("Enter a title for this list:");
+                string listTitle = Console.ReadLine();
+
+                while (saveOption == "n")
+                {
+                    string ItemChoice = "y";
+                    string mealChoice = "y";
+
+                    Console.WriteLine("Would you like to enter a MEAL or ITEMS?");
+                    string mealItemOption = Console.ReadLine();
+
+                    if (mealItemOption != "MEAL")
+                    {
+                        while (ItemChoice == "y")
+                        {
+
+                            Console.WriteLine("Enter an Item:");
+                            string itemName = Console.ReadLine();
+                            Console.WriteLine("Enter Quantity:");
+                            int quantity = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Secton: Produce(1), Meat(2), Dairy(3) Bakery(4), Frozen(5), Other(6):");
+                            int section = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Price");
+                            float price = float.Parse(Console.ReadLine());
+
+                            myActions.addItems(cmd, itemName, quantity, null, listTitle, sections[section - 1], price);
+
+                            Console.WriteLine("Would you like to ADD another item (y/n)?");
+                            ItemChoice = Console.ReadLine();
+                        }
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("Enter the title of this meal:");
+                        string mealTitle = Console.ReadLine();
+                        while (mealChoice == "y")
+                        {
+                            Console.WriteLine("Enter an Item:");
+                            string itemName = Console.ReadLine();
+                            Console.WriteLine("Enter Quantity:");
+                            int quantity = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter Secton: Produce(1), Meat(2), Dairy(3) Bakery(4), Frozen(5), Other(6):");
+                            int section = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Price");
+                            float price = float.Parse(Console.ReadLine());
+
+                            myActions.addItems(cmd, itemName, quantity, mealTitle, listTitle, sections[section - 1], price);
+                            myActions.addMeals(cmd, mealTitle, listTitle, 0);
+
+                            Console.WriteLine("Would you like to ADD another item to this meal (y/n)?");
+                            mealChoice = Console.ReadLine();
+                        }
+                    }
+
+
+                    Console.WriteLine("Are you ready to save and exit list? (y/n)");
+                    saveOption = (Console.ReadLine());
+                }
+
+
+                Console.WriteLine("Enter any notes for this list:");
+                string notes = Console.ReadLine();
+                Console.WriteLine("List has been saved.");
+                string date = DateTime.UtcNow.ToString("MM-dd-yyyy");
+                myActions.addList(cmd, listTitle, notes, date, 0);
+
+            }
+            else
+            {
+                Console.WriteLine("Please choose one of the following saved lists:");
+                myActions.readLists(cmd);
+                string listTitleOp = Console.ReadLine();
+                myActions.readListItems(cmd, listTitleOp);
+
+            }
+            Console.WriteLine("Would you like to exit? y/n");
+            exitOption = Console.ReadLine();
+        }
+
+        myActions.readItems(cmd);
+
+        return 0;
+    }
+
+}
+
+
